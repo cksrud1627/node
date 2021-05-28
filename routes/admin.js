121b673a626980a9aa31267c5db9eabbf9a5930b
cksrud1627 <cksrud1627@naver.com>
2021-05-28 11:22:27 +0900

    이력서 수정

--- routes/admin.js
+++ routes/admin.js
@@ -15,8 +15,15 @@ router.get("/", (req,res,next) => {
   return res.render("admin/main",params);
 });
 
+
+router.route("/profile")
+    /** 저장된 이력서 데이터 */
+    .get(async (req,res,next) => {
+      const data = await resume.get();
+      return res.json(data);
+    })
 /** 이력서 저장 처리 */
-router.post("/profile", async (req,res,next) => {
+    .post(async (req,res,next) => {
   const result = await resume.update(req.body);
   if (!result) {
     return alert("이력서 저장에 실패하였습니다.", res);
